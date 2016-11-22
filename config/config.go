1f1a3ed254a9d21b0373161cb199411799ff42c9
Connor Atherton <c.liam.atherton@gmail.com>
2016-11-21 23:30:27 -0800

    Decode home config file

--- config/config.go
+++ config/config.go
@@ -1,5 +1,14 @@
 package config
 
+import (
+  "os"
+  "io/ioutil"
+  "fmt"
+  "errors"
+  "path/filepath"
+  "github.com/BurntSushi/toml"
+)
+
 const configFile string = ".tasks.toml"
 
 type Config struct {
@@ -7,6 +16,22 @@ type Config struct {
 	Formatter string
 }
 
+func userDir() string {
+  return os.Getenv("HOME")
+}
+
 func New() (*Config, error) {
-	return &Config{Path: "defaultPath", Formatter: "ascii"}, nil
+  conf := Config{}
+
+  // NOTE: I could use DecodeFile here but I like having to ioutil when still learning
+  data, readErr := ioutil.ReadFile(filepath.Join(userDir(), configFile))
+  _, decodeErr := toml.Decode(string(data), &conf)
+
+  if readErr != nil || decodeErr != nil {
+    fmt.Println(errors.New("Malformed config file"), readErr, decodeErr)
+    os.Exit(1)
+  }
+
+  // NOTE: Need to figure out how to set default values for fields here
+  return &conf, nil
 }
